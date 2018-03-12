f9b9b201c527d74fadc677388e0a1502162e6b25
Jerome Olvera <jerome.olvera@gmail.com>
2018-03-12 17:09:28 -0600

    updated base example

--- AutocompleteExample/App.js
+++ AutocompleteExample/App.js
@@ -1,40 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View, Modal } from 'react-native'
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import AutocompleteTextInput from './js/AutocompleteTextInput'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const fruits = [
+  'Apple',
+  'Banana',
+  'Cherry',
+  'Date',
+  'Grape',
+  'Kiwi',
+  'Mango',
+  'Pear'
+]
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+export default class App extends Component {
+  state = {
+    text: ''
+  }
+
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <AutocompleteTextInput
+          value={this.state.text}
+          onChangeText={text => this.setState({ text })}
+          placeholder='Type something...'
+          data={fruits}
+          keyExtractor={x => x}
+        />
+
+        {
+          /*
+          <Modal
+            visible
+            animationType='slide'
+            transparent={false}
+            onRequestClose={() => {
+              alert('Modal has been closed.');
+            }}>
+            <View style={{marginTop: 22}}>
+              <AutocompleteTextInput
+                value={this.state.text}
+                onChangeText={text => this.setState({ text })}
+                data={fruits}
+                keyExtractor={x => x}
+                placeholder='Type something...'
+              />
+            </View>
+          </Modal>
+          */
+        }
       </View>
-    );
+    )
   }
 }
 
@@ -43,16 +64,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
